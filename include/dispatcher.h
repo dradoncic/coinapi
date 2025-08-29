9bbe6c88f5760d4f7c82741d7c5ad316f124f582
dradoncic <deenr214@gmail.com>
2025-08-29 11:06:36 -0400

    because of templates, dispatcher logic has to be in the header

--- include/dispatcher.h
+++ include/dispatcher.h
@@ -8,9 +8,42 @@ template<size_t TickerSize, size_t OrderbookSize>
 class Dispatcher {
 public:
     Dispatcher(RingBuffer<RawMessage, TickerSize>& ticker_queue,
-                RingBuffer<RawMessage, OrderbookSize>& orderbook_queue);
+                RingBuffer<RawMessage, OrderbookSize>& orderbook_queue) :
+                ticker_queue_{ticker_queue},
+                orderbook_queue_{orderbook_queue} {};
 
-    void handle_message(const std::string& raw);
+    void handle_message(const std::string& raw)
+    {
+        auto doc = parser_.iterate(raw);
+    
+        std::string_view type;
+        if (doc["type"].get(type)) return;
+    
+        if (type == "subscriptions") return;
+    
+        std::string_view product_id;
+        if (doc["product_id"].get(product_id)) return;
+    
+        RawMessage msg;
+        msg.channel = std::string(type);
+        msg.product_id = std::string(product_id);
+        msg.payload = raw;
+    
+        ChannelType channel_type = channelMap.at(std::string(type));
+    
+        switch (channel_type) {
+            case ChannelType::TICKER:
+                ticker_queue_.push(msg);
+                break;
+            case ChannelType::SNAPSHOT:
+            case ChannelType::L2UPDATE:
+                orderbook_queue_.push(msg);
+                break;
+            default:
+                std::cerr << "WebSocket: channel not yet supported." << "\n";
+                return;
+        }
+    }
 
 private:
     RingBuffer<RawMessage, TickerSize>& ticker_queue_;
