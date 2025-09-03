9868a2cc4299d7d7d8b71b560d499afa8b99d81a
dradoncic <deenr214@gmail.com>
2025-09-02 22:03:36 -0400

    finishing up the backfill worker

--- include/workers/backfill_worker.h
+++ include/workers/backfill_worker.h
@@ -1,1 +1,27 @@
 #pragma once
+#include <thread>
+#include <atomic>
+#include <vector>
+#include "structs/trade_event.h"
+#include "structs/backfill_req.h"
+#include "states/trade_tape.h"
+#include "ringbuffer.h"
+
+class BackFillWorker {
+public:
+    BackFillWorker(TradeTape& tape, RingBuffer<BackFillRequest>& queue);
+    ~BackFillWorker();
+
+    void start();
+    void stop();
+
+private:
+    void loop();
+    void process_request(const BackFillRequest& req);
+    TradeEvent fetch_trade_from_api(const BackFillRequest& req);
+
+    TradeTape& trade_tape_;
+    RingBuffer<BackFillRequest>& back_queue_;
+    std::thread th_;
+    std::atomic<bool> running_{false};
+};
---
+++ src/backfill_worker.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+#include <algorithm>
+#include <cstdlib>
+#include "simdjson.h"
+#include "curl/curl.h"
+#include "workers/backfill_worker.h"
+
+BackFillWorker::BackFillWorker(TradeTape& tape, RingBuffer<BackFillRequest>& queue) :
+                                trade_tape_{tape},
+                                back_queue_{queue} {}
+
+BackFillWorker::~BackFillWorker() { stop(); }
+
+void BackFillWorker::start()
+{
+    running_.store(true);
+    th_ =  std::thread(&BackFillWorker::loop, this);
+}
+
+void BackFillWorker::stop()
+{
+    running_.store(false);
+    if (th_.joinable()) th_.join();
+}
+
+void BackFillWorker::loop()
+{
+    while (running_.load(std::memory_order_acquire)) {
+        BackFillRequest req;
+        if (back_queue_.pop(req)) {
+            try { process_request(req); }
+            catch (const std::exception& e) {
+                std::cerr << "[BackFill] Error: " << e.what() << "\n";
+            }
+        } else {
+            std::this_thread::sleep_for(std::chrono::microseconds(1));
+        }
+    }
+}
